7a0f8a5df17e0a81d79cd9c994f6666514784b91
Shank34 <spikbang@gmail.com>
2025-06-23 23:55:06 +0530

    description

--- server/controller/music.controller.js
+++ server/controller/music.controller.js
@@ -2,5 +2,5 @@ import asynchandler from 'express-async-handler';
 
 export  const upload=asynchandler(async (req, res) => {
 
-  res.status(200).json({ message: "File uploaded successfully.", file: req.file });
+
 })
