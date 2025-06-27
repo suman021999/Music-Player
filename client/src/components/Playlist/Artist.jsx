ccf3f830e3f26e932751c60952765b36e6b458a3
Shank34 <spikbang@gmail.com>
2025-06-28 00:06:54 +0530

    description

--- client/src/components/Playlist/Artist.jsx
+++ client/src/components/Playlist/Artist.jsx
@@ -1,53 +1,15 @@
-
-// import React from 'react'
-
-// const Artist = ({ img, title, name, isPlaying }) => { 
-//   return (
-//     <div 
-//        className='p-2 w-[600px] flex flex-row gap-4 hover:bg-background2/20 items-center'>
-//       {img ? (
-//         <img 
-//           className={`relative w-12 h-12 rounded-full object-cover  ${isPlaying ? 'animate-spin-slow' : ''}`}
-//           src={img} 
-//           alt={title || "Track cover"} 
-//         />
-//       ) : (
-//         <div className='w-12 h-12 rounded-full bg-gray-500 flex items-center justify-center'>
-//           <span className='text-white text-xs'>No Image</span>
-//         </div>
-//       )}
-      
-//       <div className='flex justify-center flex-col overflow-hidden'>
-//         <h2 className='text-lg font-medium text-white text-ellipsis overflow-hidden whitespace-nowrap'>
-//           {title || "Unknown Track"}
-//         </h2>
-//         <p className='text-sm text-gray-200 text-ellipsis overflow-hidden whitespace-nowrap'>
-//           {name || "Unknown Artist"}
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Artist;
-
-
 import React from 'react'
 
 const Artist = ({ img, title, name, isPlaying }) => { 
   return (
     <div 
-      className='p-2 w-[600px] flex flex-row gap-4 hover:bg-background2/20 items-center'>
+       className='p-2 w-[600px] flex flex-row gap-4 hover:bg-background2/20 items-center'>
       {img ? (
-        <>
-          <img 
-            className={`cdplayer w-12 h-12 rounded-full object-cover ${isPlaying ? 'animate-spin-slow' : ''}`}
-            src={img} 
-            alt={title || "Track cover"} 
-          />
-          
-          
-        </>
+        <img 
+          className={`relative w-12 h-12 rounded-full object-cover  ${isPlaying ? 'animate-spin-slow' : ''}`}
+          src={img} 
+          alt={title || "Track cover"} 
+        />
       ) : (
         <div className='w-12 h-12 rounded-full bg-gray-500 flex items-center justify-center'>
           <span className='text-white text-xs'>No Image</span>
@@ -67,4 +29,10 @@ const Artist = ({ img, title, name, isPlaying }) => {
 };
 
 export default Artist;
+
+
+
+
+
+
 
