3cd7b4c0feaa3f77a3700170d8f15385d06c57a9
Shank34 <spikbang@gmail.com>
2025-06-30 22:18:44 +0530

    description

--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { GoHome } from "react-icons/go";
+import { PiPlaylistFill } from "react-icons/pi";
 
 function Sidebar() {
   const [isOpen, setIsOpen] = useState(true);
@@ -13,7 +15,7 @@ function Sidebar() {
 
   return (
     <div className={`text-color h-[85vh] ${isOpen ? "w-[20vw]" : "w-[60px]"} bg-background text-3xl transition-all duration-300`}>
-      <div className="flex items-center p-4 ">
+      <div className="flex items-center p-4">
         <GiHamburgerMenu className="cursor-pointer" onClick={toggleSidebar}/>
         
         {isOpen && (
@@ -23,39 +25,63 @@ function Sidebar() {
         )}
       </div>
 
-      {isOpen && (
-        <div className="flex flex-col gap-y-4 w-full mt-10">
-          <div className="flex justify-center items-center w-full">
+      {/* Search Section */}
+      <div className="flex justify-center items-center w-full mt-4">
+        {isOpen ? (
+          <div className="relative w-[18vw]">
             <input
               type="text"
-              className="relative bg-[#807d7d75] w-[18vw] p-6 h-8 text-white text-sm rounded-lg shadow-lg focus:outline-none overflow-hidden"
+              className="bg-[#807d7d75] w-full p-6 h-8 text-white text-sm rounded-lg shadow-lg focus:outline-none"
               placeholder="Search bar"
             />
-            <CiSearch className="2xl:left-60 xl:left-52 lg:left-36 md:left-28 absolute bg-[#514e4e4c] hover:bg-[#514e4e9a] p-2 rounded-lg" />
+            <CiSearch className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-[#514e4e4c] hover:bg-[#514e4e9a] p-2 rounded-lg" />
           </div>
-
-          <div className="flex justify-center items-center w-full">
-            <Link
-              to="/"
-              className="bg-[#807d7d75] w-[18vw] p-6 h-8 text-white text-sm flex items-center rounded-lg shadow-lg focus:outline-none"
-            >
-              Home
-            </Link>
+        ) : (
+          <div className="flex justify-center w-full">
+            <CiSearch className="cursor-pointer hover:text-[#b6acac]" onClick={toggleSidebar}/>
           </div>
+        )}
+      </div>
 
-          <div className="flex justify-center items-center w-full">
-            <Link
+      {/* Home Section */}
+      <div className="flex justify-center items-center w-full mt-4">
+        {isOpen ? (
+          <Link
+            to="/"
+            className="bg-[#807d7d75] w-[18vw] p-6 h-8 text-white text-sm flex items-center rounded-lg shadow-lg focus:outline-none"
+          >
+            <GoHome className="mr-2" />
+            Home
+          </Link>
+        ) : (
+          <div className="flex justify-center w-full">
+            <Link  to="/"><GoHome className="cursor-pointer hover:text-[#b6acac]"/></Link>
+          </div>
+        )}
+      </div>
+
+       {/* playlist Section */}
+      <div className="flex justify-center items-center w-full mt-4">
+        {isOpen ? (
+          <Link
               to="/playlist"
               className="bg-[#807d7d75] w-[18vw] p-6 h-8 text-white text-sm flex items-center rounded-lg shadow-lg focus:outline-none"
             >
+            <PiPlaylistFill className="mr-2" />
               Playlist
             </Link>
+        ) : (
+          <div className="flex justify-center w-full">
+            <Link to="/playlist"><PiPlaylistFill className="cursor-pointer hover:text-[#b6acac]"/></Link>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+
+      
+
+     
     </div>
   );
 }
 
 export default Sidebar;
-
