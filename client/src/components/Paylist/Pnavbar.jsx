5182fb31a7521024e6e4140adcee57a0d3dd8972
Shank34 <spikbang@gmail.com>
2025-07-01 15:56:42 +0530

    description

--- client/src/components/Paylist/Pnavbar.jsx
+++ client/src/components/Paylist/Pnavbar.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+// import React from "react";
 
-const Pnavbar = () => {
-  return (
-    <div className="text-color h-[20vh] w-[80vw]  text-3xl">
-      <div className="grid grid-cols-3 h-[15vh]">
-        <div className="flex justify-center items-center"><h1 className="text-4xl tracking-[6px] font-bold font-Poppins">Playlist</h1></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-  );
-};
+// const Pnavbar = () => {
+//   return (
+//     <div className="text-color h-[20vh] w-[80vw]  text-3xl">
+//       <div className="grid grid-cols-3 h-[15vh]">
+//         <div className="flex justify-center items-center"><h1 className="text-4xl tracking-[6px] font-bold font-Poppins">Playlist</h1></div>
+//         <div></div>
+//         <div></div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Pnavbar;
+// export default Pnavbar;
--- client/src/pages/Mainpage.jsx
+++ client/src/pages/Mainpage.jsx
@@ -1,28 +1,67 @@
 
+// import React, { useState } from 'react';
+// import Sidebar from '../Sidebar';
+// import Player from '../components/Playlist/Player';
+// import Homebar from './HBar';
+// // import Playbar from './PBar';
+// import { Routes, Route } from 'react-router-dom';
+
+// const Mainpage = () => {
+//   const [currentTrack, setCurrentTrack] = useState(null);
+
+//   return (
+//     <section className='h-screen text-color font-Poppins bg-background'>
+//       <div className='grid grid-cols-5'>
+//         <Sidebar currentTrack={currentTrack} setCurrentTrack={setCurrentTrack}/>
+//         <Routes>
+//           <Route path='/' element={<Homebar setCurrentTrack={setCurrentTrack} />} />
+//           {/* <Route path='/playlist' element={<Playbar />} /> */}
+//         </Routes>
+//       </div>
+//       {/* Pass both currentTrack and setCurrentTrack to Player */}
+//       <Player currentTrack={currentTrack} setCurrentTrack={setCurrentTrack} />
+//     </section>
+//   );
+// };
+
+// export default Mainpage;
+
+
+
+
 import React, { useState } from 'react';
 import Sidebar from '../Sidebar';
 import Player from '../components/Playlist/Player';
 import Homebar from './HBar';
-import Playbar from './PBar';
 import { Routes, Route } from 'react-router-dom';
 
 const Mainpage = () => {
   const [currentTrack, setCurrentTrack] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   return (
     <section className='h-screen text-color font-Poppins bg-background'>
       <div className='grid grid-cols-5'>
-        <Sidebar />
+        <Sidebar 
+          currentTrack={currentTrack} 
+          setCurrentTrack={setCurrentTrack}
+          onSearch={handleSearch}
+        />
         <Routes>
-          <Route path='/' element={<Homebar setCurrentTrack={setCurrentTrack} />} />
-          <Route path='/playlist' element={<Playbar />} />
+          <Route 
+            path='/' 
+            element={<Homebar setCurrentTrack={setCurrentTrack} searchQuery={searchQuery} />} 
+          />
+          {/* <Route path='/playlist' element={<Playbar />} /> */}
         </Routes>
       </div>
-      {/* Pass both currentTrack and setCurrentTrack to Player */}
       <Player currentTrack={currentTrack} setCurrentTrack={setCurrentTrack} />
     </section>
   );
 };
 
 export default Mainpage;
-
